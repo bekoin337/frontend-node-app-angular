28d29797dcd53ac7cd41a0f0e02a1c63fd8ce23f
Bekoin N <desirebekoinncho@gmail.com>
2021-11-07 17:41:29 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { ShopComponent } from './shop/shop.component';
+import { SingleProductComponent } from './shop/single-product/single-product.component';
+import { AddProductComponent } from './shop/add-product/add-product.component';
+import { EditProductComponent } from './shop/edit-product/edit-product.component';
+import { CartComponent } from './shop/cart/cart.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { FooterComponent } from './partials/footer/footer.component';
+import { NotFoundComponent } from './partials/not-found/not-found.component';
+import { HeaderPageComponent } from './partials/header-page/header-page.component';
+import { QuickViewModalComponent } from './partials/modal/quick-view-modal/quick-view-modal.component';
+import { AddToCartModalComponent } from './partials/modal/add-to-cart-modal/add-to-cart-modal.component';
+import { DeleteProductModalComponent } from './partials/modal/delete-product-modal/delete-product-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SignupComponent,
+    ShopComponent,
+    SingleProductComponent,
+    AddProductComponent,
+    EditProductComponent,
+    CartComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent,
+    HeaderPageComponent,
+    QuickViewModalComponent,
+    AddToCartModalComponent,
+    DeleteProductModalComponent
   ],
   imports: [
     BrowserModule,
